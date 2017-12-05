7111e3934c9e5a36c21b3a7e2f9853bedbae82e2
Johan Lindfors <johan.lindfors@coderox.se>
2017-12-05 09:42:38 +0100

    Preparing for day 3

---
+++ 3/C#/Client.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode {
+
+
+    public static class Solution {
+        public static void Main() {
+            var input = 289326;
+
+        }
+    }
+}
