8ca56bad0e1d8c19ccd3a4085114906750c731ea
Johan Lindfors <johan.lindfors@coderox.se>
2017-12-07 05:38:35 +0100

    Prepared for day 9

---
+++ 9/C#/UnitTests2.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    [TestClass]
+    public class UnitTests2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+           
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
