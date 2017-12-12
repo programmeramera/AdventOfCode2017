aa16c69ff97f566a48b061d73b5cd65abb58e2d2
Johan Lindfors <johan.lindfors@coderox.se>
2017-12-12 22:25:20 +0100

    Day 12 part 1 solved

--- 12/C#/Client.cs
+++ 12/C#/Client.cs
@@ -6,9 +6,68 @@ using System.Collections.Generic;
 
 namespace AdventOfCode {
     
+    public class Solver {
+        private Dictionary<int, List<int>> programs;
+        private List<int> visitedPrograms;
+
+        public Solver(){
+            programs = new Dictionary<int,List<int>>();
+        }
+
+        public int CountProgramsThatReach0(string input) {
+            ParseInput(input);
+            int count = 0;
+            foreach(var program in programs){
+                visitedPrograms = new List<int>();
+                if(CanReachDestination(program.Key,0)){
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void ParseInput(string input){
+            var lines = input.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                ParseLine(line);
+            }
+        }
+
+        private void ParseLine(string line){
+            var instructions = line.Split(new char[] {' ', '<', '-', '>', ','}, StringSplitOptions.RemoveEmptyEntries);
+            var id = int.Parse(instructions[0]);
+            var connections = new List<int>();
+            for(int i = 1; i<instructions.Length; i++){
+                connections.Add(int.Parse(instructions[i]));
+            }
+
+            programs.Add(id, connections);
+        }
+
+        private bool CanReachDestination(int index, int destination){
+            if(index == destination){
+                return true;
+            }
+            if(visitedPrograms.Contains(index)){
+               return false; 
+            } else {
+                visitedPrograms.Add(index);
+            }
+            foreach(var pipe in programs[index]){
+                if(CanReachDestination(pipe, destination)){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
     public static class Solution {
         public static void Main() {
-            //var input = System.IO.File.ReadAllText("../input.txt");
+            var input = System.IO.File.ReadAllText("../input.txt");
+            var solver = new Solver();
+            Console.WriteLine(solver.CountProgramsThatReach0(input));
         }
     }
 }
--- 12/C#/UnitTests1.cs
+++ 12/C#/UnitTests1.cs
@@ -7,14 +7,27 @@ namespace AdventOfCode
     [TestClass]
     public class UnitTests1
     {
+        string INPUT =
+@"0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+
         [TestMethod]
         public void TestMethod1()
         {
             // Arrange
-           
+           var solver = new Solver();
+           var expected = 6;
+
             // Act
+            var actual = solver.CountProgramsThatReach0(INPUT);
 
             // Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
