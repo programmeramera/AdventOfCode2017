8650ed3e945b333f0df6ad57ee6996fd0e6234ba
Johan Lindfors <johan.lindfors@coderox.se>
2017-12-12 21:44:19 +0100

    Day 11 part 2 solved

--- 11/C#/Client.cs
+++ 11/C#/Client.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 namespace AdventOfCode {
     
     public static class HexagonMap {
+        static int maxX = 0;
+        static int minX = 0;
+        static int maxY = 0;
+        static int minY = 0;
 
         public static int ClosestDistance(string input){
             var x = 0;
@@ -22,15 +26,27 @@ namespace AdventOfCode {
                     case("sw"): y++; x--; break;
                     default: break;
                 }
+
+                if(x > maxX) maxX = x;
+                if(x < minX) minX = x;
+                if(y > maxX) maxY = y;
+                if(y < minY) minY = y;
             }
 
             return Math.Max(x,y);
+        }
+
+        public static int GetMaxDistance() {
+            return Math.Max(
+                Math.Max(maxX,maxY), 
+                Math.Abs(Math.Min(minX, minY)));
         }
     }
     public static class Solution {
         public static void Main() {
             var input = System.IO.File.ReadAllText("../input.txt");
             Console.WriteLine(HexagonMap.ClosestDistance(input));
+            Console.WriteLine(HexagonMap.GetMaxDistance());
         }
     }
 }
