520637f4c9f7a66f51592f5384d0462331e9f5a0
Johan Lindfors <johan.lindfors@coderox.se>
2017-12-12 21:48:52 +0100

    Day 12 init

---
+++ 12/C#/Client.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace AdventOfCode {
+    
+    public static class Solution {
+        public static void Main() {
+            //var input = System.IO.File.ReadAllText("../input.txt");
+        }
+    }
+}
